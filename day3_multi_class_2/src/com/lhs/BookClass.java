7bd2af75f21f233e870e3558fd2832ca6a2b5b2f
Lee-HyunSoo <cris_lx@naver.com>
2023-06-27 12:00:31 +0900

    day3_multi_class_2

--- day3_multi_class_2/src/com/lhs/BookClass.java
+++ day3_multi_class_2/src/com/lhs/BookClass.java
@@ -3,7 +3,7 @@ package com.lhs;
 public class BookClass {
 	String title = null;	// 책 제목
 	String author = null;	// 저자
-	String press = null;	// 출판사
+	private String press = null;	// 출판사
 	int price = 0;			// 정가
 	String image = null;	// 책표지
 	int dc = 0;				// 할인율
@@ -15,9 +15,17 @@ public class BookClass {
 	public BookClass(String title, String author, String press, int price, String image, int dc) {
 		this.title = title;
 		this.author = author;
-		this.press = press;
+		this.setPress(press);
 		this.price = price;
 		this.image = image;
 		this.dc = dc;
+	}
+
+	public String getPress() {
+		return press;
+	}
+
+	public void setPress(String press) {
+		this.press = press;
 	}
 }
