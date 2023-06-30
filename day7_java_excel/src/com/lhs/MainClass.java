fb67abf6b84627d32dc7581759458d85908f47f9
Lee-HyunSoo <cris_lx@naver.com>
2023-06-30 17:25:22 +0900

    day7_java_excel

--- day7_java_excel/src/com/lhs/MainClass.java
+++ day7_java_excel/src/com/lhs/MainClass.java
@@ -3,6 +3,11 @@ package com.lhs;
 import com.lhs.xls.ExcelMaker;
 import com.lhs.xls.ExcelReader;
 
+/*
+ * 외부 라이브러리 가져오기
+ * 1. 클래스파일 -> Build path -> Configure Build path -> Libraries -> add external jars
+ * 2. module-info : requires 외부라이브러리명
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
